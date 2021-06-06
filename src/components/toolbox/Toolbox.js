006c25189fa737a1cf18ba431adabbd4e71a93ef
Jeremy Dombrowski <jeremy@meatflavour.dev>
2021-06-06 09:46:07 -0700

    Responsive Toolbox and refactor

--- src/components/toolbox/Toolbox.js
+++ src/components/toolbox/Toolbox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Type from '../icons/Type';
 
-import { Nav, Button, DropButton, ResponsiveContext } from 'grommet';
+import { Nav, Button, DropButton } from 'grommet';
 import { Cursor, Edit, StatusPlaceholder } from 'grommet-icons';
 import styled from 'styled-components';
 import useResponsive from '../../hooks/useResponsive';
@@ -10,40 +10,78 @@ const ToolButton = styled(Button)`
   border-radius: 10px;
 `;
 
-function Toolbox({ add }) {
-  const thisContext = useResponsive();
-
+function ToolboxNavRow({ responsive, add, handleClick }) {
+  const responsiveLabel = (labelText) => {
+    return responsive.value >= 4 ? labelText : null;
+  };
   return (
-    <ResponsiveContext.Consumer>
-    {(responsive) =>
-      responsive === 'xsmall' ? (
-        <Nav direction="column" gap="xsmall">
-          <DropButton
+    <Nav direction="row" gap="small">
+      <ToolButton
+        plain={false}
+        icon={<Cursor />}
+        onClick={() => handleClick('Arrow')}
+        primary={true}
+        label={responsiveLabel('Arrow')}
+      />
+      <ToolButton plain={false} icon={<Edit />} onClick={() => handleClick('Draw')} label={responsiveLabel('Pencil')} />
+      <ToolButton
+        plain={false}
+        icon={<StatusPlaceholder />}
+        onClick={() => add(window.canvas)}
+        label={responsiveLabel('Shape')}
+        size="medium"
+      />
+      <ToolButton
+        plain={false}
+        icon={<Type />}
+        onClick={() => {
+          handleClick('Text');
+        }}
+        label={responsiveLabel('Text')}
+        size="large"
+      />
+    </Nav>
+  );
+}
+
+function ToolboxNavCol({ add, handleClick }) {
+  return (
+    <DropButton
+      plain={false}
+      icon={<Cursor />}
+      onClick={() => handleClick('Arrow')}
+      primary={true}
+      dropAlign={{ top: 'bottom' }}
+      dropContent={
+        <>
+          <Button plain={false} icon={<Edit />} onClick={() => handleClick('Draw')} />
+          <Button plain={false} icon={<StatusPlaceholder />} onClick={() => add(window.canvas)} />
+          <Button
             plain={false}
-            icon={<Cursor />}
-            onClick={() => console.log('Cursor')}
-            primary={true}
-            dropAlign={{ top: 'bottom' }}
-            dropContent={
-              <>
-                <Button plain={false} icon={<Edit />} onClick={() => console.log('Draw')} />
-                <Button plain={false} icon={<StatusPlaceholder />} onClick={() => add(window.canvas)} />
-                <Button plain={false} icon={<Type />} onClick={() => { console.log('Text'); }} />
-              </>
-            }
+            icon={<Type />}
+            onClick={() => {
+              handleClick('Text');
+            }}
           />
-        </Nav>
-      ) : (
-        <Nav direction="row" gap={['xsmall', 'small'].includes(responsive) ? 'small' : 'medium'}>
-          <ToolButton plain={false} icon={<Cursor />} onClick={() => console.log('Cursor')} primary={true} label="Arrow" />
-          <ToolButton plain={false} icon={<Edit />} onClick={() => console.log('Draw')} label="Pencil" />
-          <ToolButton plain={false} icon={<StatusPlaceholder />} onClick={() => add(window.canvas)} label="Shape" size="medium" />
-          <ToolButton plain={false} icon={<Type />} onClick={() => { console.log('Text'); }} label="Text" size="large" />
-        </Nav>
-      )
-    }
-  </ResponsiveContext.Consumer>
-  )
+        </>
+      }
+    />
+  );
+}
+
+function Toolbox({ add }) {
+  // Get size and value relative to the window size/device and responsive context
+  const responsive = useResponsive();
+
+  const handleClick = (name) => {
+    console.log(`Click ${name}`);
+  };
+
+  return responsive.size !== 'xsmall' ? (
+    <ToolboxNavRow responsive={responsive} add={add} handleClick={handleClick} />
+  ) : (
+    <ToolboxNavCol add={add} handleClick={handleClick} />
+  );
 }
 
 export default Toolbox;
